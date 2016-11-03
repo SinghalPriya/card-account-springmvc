f8ed7f996b56284080088f5fe9182d84fbe68c36
SriramZafin <sriram.sundararajan@zafin.com>
2016-11-03 12:48:59 +0530

    Update AccountsApiController.java

--- src/main/java/io/swagger/api/AccountsApiController.java
+++ src/main/java/io/swagger/api/AccountsApiController.java
@@ -1,24 +1,17 @@
 package io.swagger.api;
 
-import io.swagger.model.AccountDetails;
-import io.swagger.model.Accounts;
-
-import io.swagger.annotations.*;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.AccountDetails;
+import io.swagger.model.Accounts;
 
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-11-02T17:32:19.191+05:30")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-11-01T18:09:28.587+05:30")
 
 @Controller
 public class AccountsApiController implements AccountsApi {
@@ -34,8 +27,18 @@ public class AccountsApiController implements AccountsApi {
     }
 
     public ResponseEntity<Accounts> getAllAccounts() {
-        // do some magic!
-        return new ResponseEntity<Accounts>(HttpStatus.OK);
+        System.out.println(" Getting here ******************************");
+        AccountDetails accountDetails=new AccountDetails();
+        accountDetails.setUserName("Maddy");
+        accountDetails.setCustomerId(1234);
+
+
+        Accounts accounts=new Accounts();
+        accounts.add(accountDetails);
+
+
+        return new ResponseEntity(accounts, HttpStatus.OK);
+        //return new ResponseEntity<Accounts>(HttpStatus.OK);
     }
 
 }
